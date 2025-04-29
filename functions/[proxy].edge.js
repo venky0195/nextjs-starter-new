442edd6969752f521379c700d38a4535516f2350
Venkatesh <venkatesh.gopinath@contentstack.com>
2025-04-29 12:00:25 +0530

    Add logs to measure latency on edge and cf

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,16 @@
-export default function handler(request, context) {
-    console.log("hello from edge");
-   return fetch(request)
- }
+export default async function handler(request, context) {
+  const edgeStart = Date.now();
+
+  // Clone and modify headers
+  const headers = new Headers(request.headers);
+  headers.set("x-start-time", edgeStart.toString());
+
+  const modifiedRequest = new Request(request, { headers });
+
+  const response = await fetch(modifiedRequest);
+
+  const fetchLatency = Date.now() - edgeStart;
+  console.log(`Edge Fetch Latency: ${fetchLatency} ms`);
+
+  return response;
+}
--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,15 @@
-export default function handlers(req, res) {
-    console.log("process.env",process.env)
-    console.log("This is a console")
-
-    
-    res
-    .status(200)
-    .json({ message: "helloworld!" })
-}
+export default function handler(req, res) {
+    const startTimeHeader = req.headers["x-start-time"];
+    let latency = null;
+  
+    if (startTimeHeader) {
+      const start = parseInt(startTimeHeader, 10);
+      latency = Date.now() - start;
+      console.log(`Latency from edge to cloud function: ${latency} ms`);
+    } else {
+      console.log("x-start-time header not found");
+    }
+  
+    res.status(200).json({ message: "helloworld!", latency: latency ? `${latency} ms` : "unknown" });
+  }
+  
